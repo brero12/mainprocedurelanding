f3c4642180aeac5fd8333c5c169fd625f43dc705
Bryan R <8272332+brero12@users.noreply.github.com>
2025-08-29 12:56:32 -0700

    Modifying lead descriptions for all

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,20 +1,19 @@
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid2';
-import { useTheme } from '@mui/system';
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid2";
+import { useTheme } from "@mui/system";
 
 const userTestimonials = [
   {
     avatar: <Avatar alt="Bryan Rodriguez" src="/static/images/avatar/1.jpg" />,
-    name: 'Bryan Rodriguez',
-    occupation: 'Software Engineer Lead',
-    testimonial:
-      `A highly accomplished and dedicated Software Development Lead with over 10 years of experience 
+    name: "Bryan Rodriguez",
+    occupation: "Software Engineer Lead",
+    testimonial: `A highly accomplished and dedicated Software Development Lead with over 10 years of experience 
       driving project success and enhancing organizational goals. This professional is a proven leader in architecting 
       robust solutions and guiding development teams. He possess a deep technical expertise across 
       a wide range of technologies. With a strong focus on collaboration and clear communication, 
@@ -22,47 +21,56 @@ const userTestimonials = [
   },
   {
     avatar: <Avatar alt="Jorge Parra" src="/static/images/avatar/2.jpg" />,
-    name: 'Jorge Parra',
-    occupation: 'Product Designer Lead',
-    testimonial: `With 20+ years in technology and a Master's in AI and Systems Engineering, he combines deep technical expertise with strong leadership skills, transforming business needs into innovative solutions.
-      Specializing in data analysis and Machine Learning using Python, Azure, TensorFlow, and Keras, he delivers comprehensive solutions from infrastructure to cloud platforms. His experience with SCRUM methodologies 
-      and 8+ years in Data Analytics ensures successful project delivery that drives digital transformation.
-      His expertise in data networks creates robust, scalable solutions with optimal performance. As a natural leader with strong interpersonal skills, he's both a technical expert and strategic innovator committed to excellence.`,
+    name: "Jorge Parra",
+    occupation: "Product Designer Lead",
+    testimonial: `A seasoned technology leader with a Master's degree in Artificial Intelligence and Systems Engineering, 
+    he has over two decades of experience transforming business needs into innovative tech solutions. 
+    His expertise includes data analysis, Machine Learning model development, and cloud solutions using 
+    Python, Azure, TensorFlow, and Keras. With a strong background in data networks and infrastructure, 
+    he applies an agile, data-driven approach to deliver scalable 
+    and robust solutions that drive digital transformation.`,
   },
   {
     avatar: <Avatar alt="Omar Pino" src="/static/images/avatar/3.jpg" />,
-    name: 'Omar Pino',
-    occupation: 'Innovation Lead',
-    testimonial: 'Lorem ipsum...',
+    name: "Omar Pino",
+    occupation: "Innovation Lead",
+    testimonial: `With 20+ years in technology and a Master's in AI and Systems Engineering, 
+      he combines deep technical expertise with strong leadership skills, transforming business needs 
+      into innovative solutions. Specializing in data analysis and Machine Learning using Python, Azure, TensorFlow, 
+      and Keras, he delivers comprehensive solutions from infrastructure to cloud platforms. His experience 
+      with SCRUM methodologies and 8+ years in Data Analytics ensures successful project delivery that drives digital 
+      transformation. His expertise in data networks creates robust, scalable solutions with optimal performance. 
+      As a natural leader with strong interpersonal skills, he's both a technical expert and strategic 
+      innovator committed to excellence.`,
   },
 ];
 
 const whiteLogos = [
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628e8573c43893fe0ace_Sydney-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d520d0517ae8e8ddf13_Bern-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f46794c159024c1af6d44_Montreal-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e891fa22f89efd7477a_TerraLight.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a09d1f6337b1dfed14ab_colorado-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5caa77bf7d69fb78792e_Ankara-white.svg',
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628e8573c43893fe0ace_Sydney-white.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d520d0517ae8e8ddf13_Bern-white.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f46794c159024c1af6d44_Montreal-white.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e891fa22f89efd7477a_TerraLight.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a09d1f6337b1dfed14ab_colorado-white.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5caa77bf7d69fb78792e_Ankara-white.svg",
 ];
 
 const darkLogos = [
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg',
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg",
+  "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg",
 ];
 
 const logoStyle = {
-  width: '64px',
+  width: "64px",
   opacity: 0.3,
 };
 
 export default function Testimonials() {
   const theme = useTheme();
-  const logos = theme.palette.mode === 'light' ? darkLogos : whiteLogos;
+  const logos = theme.palette.mode === "light" ? darkLogos : whiteLogos;
 
   return (
     <Container
@@ -70,28 +78,28 @@ export default function Testimonials() {
       sx={{
         pt: { xs: 4, sm: 12 },
         pb: { xs: 8, sm: 16 },
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         gap: { xs: 3, sm: 6 },
       }}
     >
       <Box
         sx={{
-          width: { sm: '100%', md: '60%' },
-          textAlign: { sm: 'left', md: 'center' },
+          width: { sm: "100%", md: "60%" },
+          textAlign: { sm: "left", md: "center" },
         }}
       >
         <Typography
           component="h2"
           variant="h4"
           gutterBottom
-          sx={{ color: 'text.primary' }}
+          sx={{ color: "text.primary" }}
         >
           Who we are
         </Typography>
-        <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body1" sx={{ color: "text.secondary" }}>
           Get to know the team behind our success. We're committed to providing
           you with quality, innovation, and reliable support, ensuring a
           comfortable and productive partnership.
@@ -99,13 +107,17 @@ export default function Testimonials() {
       </Box>
       <Grid container spacing={2}>
         {userTestimonials.map((testimonial, index) => (
-          <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index} sx={{ display: 'flex' }}>
+          <Grid
+            size={{ xs: 12, sm: 6, md: 4 }}
+            key={index}
+            sx={{ display: "flex" }}
+          >
             <Card
               variant="outlined"
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
                 flexGrow: 1,
               }}
             >
@@ -113,16 +125,16 @@ export default function Testimonials() {
                 <Typography
                   variant="body1"
                   gutterBottom
-                  sx={{ color: 'text.secondary' }}
+                  sx={{ color: "text.secondary" }}
                 >
                   {testimonial.testimonial}
                 </Typography>
               </CardContent>
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
                 }}
               >
                 <CardHeader
